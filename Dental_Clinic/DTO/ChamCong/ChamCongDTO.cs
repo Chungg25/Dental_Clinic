01a611af1aedb8cfc547b45f07c4d10f98a37ce1
kiet <chungkiet31@gmail.com>
2024-10-31 18:00:48 +0700

    lich lam viec

--- Dental_Clinic/DTO/ChamCong/ChamCongDTO.cs
+++ Dental_Clinic/DTO/ChamCong/ChamCongDTO.cs
@@ -15,11 +15,23 @@ namespace Dental_Clinic.DTO.ChamCong
         private bool gioiTinh;
         private string email;
         private int trangThai;
+        private string ghiChu;
+        private string gioVao;
+        private string gioRa;
+        private string sĐT;
+        private string diaChi;
 
         public string Ngay { get => ngay; set => ngay = value; }
         public int Ca { get => ca; set => ca = value; }
         public int MaNguoiDung { get => maNguoiDung; set => maNguoiDung = value; }
         public string HoTen { get => hoTen; set => hoTen = value; }
         public int TrangThai { get => trangThai; set => trangThai = value; }
+        public string GhiChu { get => ghiChu; set => ghiChu = value; }
+        public string Email { get => email; set => email = value; }
+        public bool GioiTinh { get => gioiTinh; set => gioiTinh = value; }
+        public string GioVao { get => gioVao; set => gioVao = value; }
+        public string GioRa { get => gioRa; set => gioRa = value; }
+        public string SĐT { get => sĐT; set => sĐT = value; }
+        public string DiaChi { get => diaChi; set => diaChi = value; }
     }
 }
--- Dental_Clinic/DTO/LichLamViec/LichLamViecDTO.cs
+++ Dental_Clinic/DTO/LichLamViec/LichLamViecDTO.cs
@@ -9,15 +9,11 @@ namespace Dental_Clinic.DTO.LichLamViec
     internal class LichLamViecDTO
     {
         private int maNguoiDung;
-        private string ngay;
-        private int ca;
         private string hoTen;
         private bool gioiTinh;
         private string email;
         private int soCa;
 
-        public string Ngay { get => ngay; set => ngay = value; }
-        public int Ca { get => ca; set => ca = value; }
         public int MaNguoiDung { get => maNguoiDung; set => maNguoiDung = value; }
         public string HoTen { get => hoTen; set => hoTen = value; }
         public bool GioiTinh { get; set; }
